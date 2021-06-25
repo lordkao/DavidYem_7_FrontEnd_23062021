ea5d698b14a8062d1249a000e105300b1238f9e5
David Yem <david13200arles@hotmail.fr>
2021-06-25 19:13:44 +0200

    publications actives

--- src/forums.js
+++ src/forums.js
@@ -4,6 +4,7 @@ const btnProfil = document.getElementById("btn-profil")
 const multimedia = document.getElementById("multimedia")
 const chat = document.getElementById("chat")
 const chatWindow = document.getElementById('chat-window')
+const publicationWindow = document.getElementById('publication')
 
 const borderBlock = ()=>{
     btnMultimedia.style.borderBottom = "1px grey solid"
@@ -37,10 +38,11 @@ btnProfil.addEventListener("click",function(e){
     btnProfil.style.borderBottom = "transparent"
 })
 
-const url ='http://localhost:3000/chat'
+const urlChat ='http://localhost:3000/chat'
+const urlPublications ='http://localhost:3000/publications'
 
 function showMessage(){
-    fetch(url)
+    fetch(urlChat)
     .then(function(res){
         if(res.ok){
             return res.json()
@@ -71,5 +73,43 @@ function showMessage(){
         console.log({ message: error})
     })
 }
-
 showMessage()
+
+function showPublications(){
+    fetch(urlPublications)
+    .then(function(res){
+        if(res.ok){
+            return res.json()
+        }
+    })
+    .then(function(responses){
+        console.log('publication :')
+        console.log(responses)
+
+        for(let response of responses){
+            const auteur = document.createElement('h3')
+                auteur.classList.add('publication__contenue--auteur')
+                auteur.innerText = `${response.auteur}`
+
+            const publication = document.createElement('div')
+                publication.classList.add('publication__contenue--photo')
+
+            const image = document.createElement('img')
+
+            const texte = document.createElement('p')
+                texte.classList.add('publication__contenue--texte')
+                texte.innerText = `${response.texte}`
+
+            publicationWindow.appendChild(auteur)
+            publicationWindow.appendChild(publication)
+            publicationWindow.appendChild(texte)
+            publication.appendChild(image)
+
+
+        }
+    })
+    .catch(function(error){
+        console.log({ error })
+    })
+}
+showPublications()
