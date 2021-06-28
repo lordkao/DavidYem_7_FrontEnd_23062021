a8d304701492aa5fbc1294768c20922537e8d6e6
David Yem <david13200arles@hotmail.fr>
2021-06-28 21:48:18 +0200

    index correctif

--- src/index.js
+++ src/index.js
@@ -30,55 +30,111 @@ signup.addEventListener("click",function(e){
 
 let btnValidationConnexion = document.getElementById("validation-connexion")
 let btnValidationCreation = document.getElementById("validation-creation")
-
+/*Redirection vers la page forum.html.*/
+/*************************************************/
 function forum(){
     document.location.href="forum.html"
 }
-/*Validation du formulaire via le bouton valider */
+/*Déclaration des inputs de connection.*/
+/*************************************************/
+let emailConnect = document.getElementById('email-connect')
+let passwordConnect = document.getElementById('password-connect')
 
+/*Validation du formulaire via le bouton valider */
+/*************************************************/
 btnValidationConnexion.addEventListener("click",function(e){
-    e.preventDefault
-    
+    e.preventDefault()
+    let form = {
+        email : emailConnect.value,
+        password : passwordConnect.value
+    }  
+    if(emailConnect.value == '' || null || undefined){
+        alert('il manque l\'email !')
+    }
+    else if(passwordConnect.value == '' || null || undefined){
+        alert('il manque le mot de passe !')
+    }
+    else{
+        /*console.log(form)*/
+        fetch(urlUsersConnect,{
+            method: "POST",
+            headers:{
+                "Accept":"application/json",
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(form)
+        })
+        .then(function(res){
+            if(res.ok){
+                return res.json()
+            }
+        })
+        .then(function(response){
+            console.log(response)
+            if(response.userId && response.token){
+                forum()
+            }
+        })
+        .catch(function(err){
+            alert(err)
+        })
+    }
 })
 
-/*Validation du formulaire via le bouton Inscription */
+/*Déclaration des inputs de formulaire d'inscription*/
+/*************************************************/
 const nom = document.getElementById('nom')
 const prenom = document.getElementById('prenom')
 const emailSignup = document.getElementById('email-inscription')
 const passwordSignup = document.getElementById('password-inscription')
 
+/*Validation du formulaire via le bouton Inscription */
+/*************************************************/
 btnValidationCreation.addEventListener("click",function(e){
+    e.preventDefault()
     let form = { 
         nom: nom.value,
         prenom: prenom.value,
         email: emailSignup.value,
         password : passwordSignup.value,
     }
-    /*console.log(form)*/
-    fetch(urlUsersSignup,{
-        method: "POST",
-        headers:{
-            "Accept":"application/json",
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(form)
-        }
-    )
-    .then(function(res){
-        if(res.ok){
-            return res.json()
-        }
-    })
-    .then(function(response){
-        console.log(response)
-        if(response.token == ''){
-            console.log('erreur')
-        }
-        else if(response.token){
-            forum()
-        }
-    })
-    .catch(function(err){
-        alert(err)
-    })
+    if(nom.value == '' || null || undefined){
+        alert('il manque le nom')
+    }
+    else if(prenom.value == '' || null || undefined){
+        alert('il manque le prenom')
+    }
+    else if(emailSignup.value == '' || null || undefined){
+        alert('il manque l\'adresse email')
+    }
+    else if(passwordSignup.value == '' || null || undefined){
+        alert('il manque le password')
+    }
+    else{
+        console.log(form)
+        fetch(urlUsersSignup,{
+            method: "POST",
+            headers:{
+                "Accept":"application/json",
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(form)
+            }
+        )
+        .then(function(res){
+            if(res.ok){
+                return res.json()
+            }
+        })
+        .then(function(response){
+            console.log(response)
+            if(response.userId && response.token){
+                forum()
+            }
+        })
+        .catch(function(err){
+            alert(err)
+        })
+    }
+    
 })
